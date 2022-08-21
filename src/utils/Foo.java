ffb696c696d12ccaec47c4880499a88b35ac4133
David Bautista <davidbautista1206@gmail.com>
2022-08-21 17:27:33 -0400

    Added greetings

---
+++ src/utils/Foo.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("Hi from GitHub");
+		System.out.println("Hi 'david1' branch, from Eclipse");
+		System.out.println("Hello worlds");
+		System.out.println("Greetings");
+	}
+
+}
