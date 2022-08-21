9f8157cc250e88acf35a64d2a698fb90fd517a40
David Bautista <davidbautista1206@gmail.com>
2022-08-21 17:11:13 -0400

    Added another print statement

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -5,6 +5,7 @@ public class Foo {
 	public static void main(String[] args) {
 		System.out.println("howdy");
 		System.out.println("Hi from GitHub");
+		System.out.println("Hi 'david1' branch, from Eclipse");
 	}
 
 }
